801a4771dfc8eec3874ac04a301315a4bba92803
dmitry <dmitryure@gmail.com>
2020-03-03 15:05:44 +0300

    body parser added form listens to form data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const app = express()
 
@@ -7,8 +8,21 @@ const arr = ["hello", "zdras'te"]
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
     res.render('home', {layout: "templates/layout"})
+})
+
+app.post('/form', function (req, res){
+    console.log(req.body)
+    res.status(200)
+})
+
+app.get('/form', function(req, res) {
+    res.render('form', {layout: 'templates/layout'})
 })
 
 app.get('/users', function (req, res) {
